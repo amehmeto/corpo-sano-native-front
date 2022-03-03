f7fa251289dc243bdbf582d7ef76dd7a7a49e622
amehmeto <42amehmeto@gmail.com>
2022-03-03 12:15:58 +0300

    wip(pact): setting up pact test runner 2

--- src/create-program/gateways/exercise.graphql.gateway.pact.ts
+++ src/create-program/gateways/exercise.graphql.gateway.pact.ts
@@ -1,26 +1,47 @@
+import { pactProvider } from '../../../.pact/setup'
+
 const path = require('path')
 import { ExerciseGateway } from './exercise.gateway.interface'
 import { Exercise } from '../entities/exercise.entity'
 import { GraphqlExerciseGateway } from './graphqlExerciseGateway'
-import { Pact } from '@pact-foundation/pact'
+import { GraphQLInteraction, Matchers, Pact } from '@pact-foundation/pact'
 
 describe('Exercise Gateway', () => {
   let exerciseGateway: ExerciseGateway
-  const pactProvider = new Pact({
-    consumer: 'ExerciseGateway',
-    provider: 'NestJs',
-    port: 1234,
-    log: path.resolve(process.cwd(), 'logs', 'pact.log'),
-    dir: path.resolve(process.cwd(), 'pacts'),
-    logLevel: 'info',
-  })
 
   const expectedBody = { id: 'whatever' }
 
-  beforeAll(async () => {
-    /*    pactProvider.setup().then(() =>
-      pactProvider.addInteraction(new Pact.GraphQLInteraction()),
-      )*/
+  beforeAll((done) => {
+    const contentTypeJsonMatcher = Matchers.term({
+      matcher: 'application\\/json; *charset=utf-8',
+      generate: 'application/json; charset=utf-8',
+    })
+
+    pactProvider
+      .setup()
+      .then(() =>
+        pactProvider.addInteraction(
+          new GraphQLInteraction()
+            .uponReceiving('a findById query')
+            .withRequest({
+              path: 'graphql',
+              method: 'POST',
+            })
+            .withOperation('')
+            .withQuery(`query qrqafsgl sdfg`)
+            .withVariables({})
+            .willRespondWith({
+              status: 200,
+              headers: {
+                'Content-Type': contentTypeJsonMatcher,
+              },
+              body: {
+                data: {},
+              },
+            }),
+        ),
+      )
+      .then(() => done())
 
     exerciseGateway = new GraphqlExerciseGateway()
   })
